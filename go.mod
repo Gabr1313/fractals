235f8f2d104aabe64842ab6da6eec39bce42acef
Gabr1313 <gabriele.baietta03@gmail.com>
2024-01-31 22:02:26 +0100

    working zoom

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/gabr1313/fractals
 
 go 1.21.6
 
-require github.com/hajimehoshi/ebiten/v2 v2.6.4
+require (
+	github.com/hajimehoshi/bitmapfont/v3 v3.0.0
+	github.com/hajimehoshi/ebiten/v2 v2.6.4
+)
 
 require (
 	github.com/ebitengine/purego v0.5.0 // indirect
@@ -12,4 +15,5 @@ require (
 	golang.org/x/mobile v0.0.0-20230922142353-e2f452493d57 // indirect
 	golang.org/x/sync v0.3.0 // indirect
 	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,51 +1,75 @@
 package main
 
 import (
-	"github.com/hajimehoshi/ebiten/v2"
 	"log"
 	"runtime"
 	"sync"
-	"time"
+	// "time"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
+
+// TODO: WHAT TO DO WITH THE LOW ZOOM PROBLEM?
+// try to use channels, with random points
+
+// TODO: MOUSE GESTURE
 
 const (
 	SCREEN_WIDTH        = 1080
 	SCREEN_HEIGHT       = 1080
 	SQRT_DOTS_PER_PIXEL = 2
-	MAX_STEP            = 128
-	STEP_PER_FRAME      = 32
 	STARTING_POINT      = complex(0, 0)
-	CENTRE              = complex(-.75, 0)
-	ZOOM_X              = float64(2.5e-0)
-	// CENTRE              = complex(-.749000099001, 0.101000001)
-	// ZOOM_X              = float64(2.5e-13) // max zoom
+
+	// CENTRE_0       = complex(-.75, 0)
+	// TODO: increase max step a loooooot but using channels, so the cpu does
+	// not explode
+	MAX_STEP       = 1024
+	STEP_PER_FRAME = 8
+	ZOOM           = 2.5e-0
+	ZOOM_DELTA     = .9
+
+	// max zoom
+	CENTRE_0 = complex(-.749000099001, 0.101000001)
+	// MAX_STEP       = 1024
+	// STEP_PER_FRAME = 1024
+	// ZOOM           = 2.5e-13
+	// ZOOM_DELTA     = .9
 
 	// below value should not be changed
-	GAME_WIDTH  = 1080 * SQRT_DOTS_PER_PIXEL
-	GAME_HEIGHT = 1080 * SQRT_DOTS_PER_PIXEL
-	ZOOM_Y      = GAME_HEIGHT * ZOOM_X / GAME_WIDTH
-	THRESHOLD   = float64(2)
+	GAME_WIDTH     = 1080 * SQRT_DOTS_PER_PIXEL
+	GAME_HEIGHT    = 1080 * SQRT_DOTS_PER_PIXEL
+	THRESHOLD      = float64(2)
+	ZOOM_DELTA_REV = 1 / ZOOM_DELTA
 )
 
 // Wikipedia palette
-var pre_palette [64]byte = [64]byte{
-	9, 1, 47, 0xff,
-	4, 4, 73, 0xff,
-	0, 7, 100, 0xff,
-	12, 44, 138, 0xff,
-	24, 82, 177, 0xff,
-	57, 125, 209, 0xff,
-	134, 181, 229, 0xff,
-	211, 236, 248, 0xff,
-	241, 233, 191, 0xff,
-	248, 201, 95, 0xff,
-	255, 170, 0, 0xff,
-	204, 128, 0, 0xff,
-	153, 87, 0, 0xff,
-	106, 52, 3, 0xff,
-	66, 30, 15, 0xff,
-	25, 7, 26, 0xff,
-}
+var (
+	pre_palette [64]byte = [64]byte{
+		9, 1, 47, 0xff,
+		4, 4, 73, 0xff,
+		0, 7, 100, 0xff,
+		12, 44, 138, 0xff,
+		24, 82, 177, 0xff,
+		57, 125, 209, 0xff,
+		134, 181, 229, 0xff,
+		211, 236, 248, 0xff,
+		241, 233, 191, 0xff,
+		248, 201, 95, 0xff,
+		255, 170, 0, 0xff,
+		204, 128, 0, 0xff,
+		153, 87, 0, 0xff,
+		106, 52, 3, 0xff,
+		66, 30, 15, 0xff,
+		25, 7, 26, 0xff,
+	}
+
+	// TODO: this should go in the game struct
+	CENTRE   = CENTRE_0
+	ZOOM_X   = ZOOM
+	ZOOM_Y   = GAME_HEIGHT * ZOOM_X / GAME_WIDTH
+	MOVEMENT = ZOOM_X * 0.01
+)
 
 // TODO: maybe it could be better to use the % operator
 func NewPalette() [][]byte {
@@ -80,6 +104,7 @@ type Game struct {
 	buf      []byte
 	palette  [][]byte
 	image    *ebiten.Image
+	keys     []ebiten.Key
 }
 
 func NewGame() *Game {
@@ -106,6 +131,18 @@ func (g *Game) ResetGame() {
 		g.buf[i*4+2] = (g.palette)[2][0]
 		g.buf[i*4+3] = (g.palette)[3][0]
 	}
+}
+
+func (g *Game) Move(direction complex128) {
+	CENTRE += direction
+	g.ResetGame()
+}
+
+func (g *Game) Zoom(zoom float64) {
+	ZOOM_X *= zoom
+	ZOOM_Y = GAME_HEIGHT * ZOOM_X / GAME_WIDTH
+	MOVEMENT = ZOOM_X * 0.01
+	g.ResetGame()
 }
 
 func (g *Game) Calculate(palette *[][]byte, stepsTodo int) {
@@ -154,12 +191,40 @@ func (g *Game) Calculate(palette *[][]byte, stepsTodo int) {
 }
 
 func (g *Game) Update() error {
+	g.keys = inpututil.AppendPressedKeys(g.keys[:0])
+	// if ebiten.IsKeyPressed(ebiten.KeyEscape) {
+	//return ebiten.Termination
+	// }
+	for _, k := range g.keys {
+		switch k {
+		case ebiten.KeyR:
+			ZOOM_X = ZOOM
+			ZOOM_Y = GAME_HEIGHT * ZOOM_X / GAME_WIDTH
+			MOVEMENT = ZOOM_X * 0.01
+			g.ResetGame()
+		case ebiten.KeyQ:
+			return ebiten.Termination
+		case ebiten.KeyH, ebiten.KeyArrowLeft:
+			g.Move(complex(-MOVEMENT, 0))
+		case ebiten.KeyJ, ebiten.KeyArrowDown:
+			g.Move(complex(0, -MOVEMENT))
+		case ebiten.KeyK, ebiten.KeyArrowUp:
+			g.Move(complex(0, MOVEMENT))
+		case ebiten.KeyL, ebiten.KeyArrowRight:
+			g.Move(complex(MOVEMENT, 0))
+		case ebiten.KeyF:
+			g.Zoom(ZOOM_DELTA)
+		case ebiten.KeyD:
+			g.Zoom(ZOOM_DELTA_REV)
+		}
+	}
+
 	if g.stepDone < MAX_STEP {
-		start := time.Now()
+		// start := time.Now()
 		g.Calculate(&g.palette, min(STEP_PER_FRAME, MAX_STEP-g.stepDone))
-		end := time.Now()
-		log.Printf("Calculation took %dms (%.2f FPS?)",
-			(end.Sub(start)).Milliseconds(), 1/(end.Sub(start)).Seconds())
+		// end := time.Now()
+		// log.Printf("Calculation took %dms (%.2f FPS?)",
+		// 	(end.Sub(start)).Milliseconds(), 1/(end.Sub(start)).Seconds())
 	}
 	return nil
 }
